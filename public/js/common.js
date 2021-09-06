e84028ea392171bcafa23d34bdff15915545f562
Jayashakthi28 <vishnu28.js@gmail.com>
2021-09-06 07:45:01 +0530

    remove import statement

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,3 @@
-import { db_get,firebaseConfig} from "./firebase-util.js";
-
-
 document.querySelector('header > img').addEventListener('click', ()=>{
     document.querySelector('.profile_menu').classList.toggle('none');
 })
