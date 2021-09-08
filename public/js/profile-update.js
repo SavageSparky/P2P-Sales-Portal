c58830d4adfcd9fa06084833751fe10c9b2f510f
Jayashakthi28 <vishnu28.js@gmail.com>
2021-09-08 14:24:35 +0530

    added basic otp logic

--- public/js/profile-update.js
+++ public/js/profile-update.js
@@ -9,6 +9,16 @@ const select_tag=document.querySelector('select');
 let user_id;
 let user_det;
 const btn=document.querySelectorAll('button');
+const otp_send_btn=document.querySelector('.phone-no-wrap button');
+const otp_cont=document.querySelector('.otp-cont');
+let otp_num;
+const otp_verify_btn=document.querySelector('.check-otp-btn');
+
+
+function otpGenerator(){
+    return Math.floor(Math.random()*(9999-1111)+1111);
+}
+
 firebase.auth().onAuthStateChanged(async (user)=>{
     if(!user){
         location.href='/index.html';
@@ -48,8 +58,8 @@ async function pinCodeAreaFiller(){
     let data=await fetch(url);
     data=await data.json();
     if (data[0]["Status"] === "Success") {
-        input_ele[6].value=data[0]['PostOffice'][0]["District"];
-        input_ele[7].value=data[0]['PostOffice'][0]["State"];
+        input_ele[7].value=data[0]['PostOffice'][0]["District"];
+        input_ele[8].value=data[0]['PostOffice'][0]["State"];
         select_tag.innerHTML='';
         data[0].PostOffice.forEach(d=>{
             if(user_det['area']!==undefined && user_det['area']===d.Name){
@@ -61,8 +71,8 @@ async function pinCodeAreaFiller(){
         });
     }
     else{
-        input_ele[6].value=null;
         input_ele[7].value=null;
+        input_ele[8].value=null;
         select_tag.innerHTML=` <option value="${null}">No Area Found</option>`;
     }
 }
@@ -70,13 +80,13 @@ async function pinCodeAreaFiller(){
 input_ele[5].addEventListener('input',()=>{
     if(input_ele[5].value.length>6){
         input_ele[5].value=null;
-        input_ele[6].value=null;
         input_ele[7].value=null;
+        input_ele[8].value=null;
         select_tag.innerHTML=` <option value="${null}">No Area Found</option>`;
     }
     if(input_ele[5].value.length<6){
-        input_ele[6].value=null;
         input_ele[7].value=null;
+        input_ele[8].value=null;
         select_tag.innerHTML=` <option value="${null}">No Area Found</option>`;
     }
     if(input_ele[5].value.length===6){
@@ -93,5 +103,47 @@ input_ele[0].addEventListener('change',()=>{
     currFiles.forEach(file=>{
         profile_pic_cont.style.background=`url(${URL.createObjectURL(file)}) no-repeat center center / cover`;
     })
+})
+
+input_ele[2].addEventListener('input',()=>{
+    if(input_ele[2].value.length===10){
+        input_ele[2].style.width='125px';
+        otp_send_btn.style.display='block';
+    }
+    else{
+        input_ele[2].style.width='190px';
+        otp_send_btn.style.display='none';
+    }
+});
+
+otp_send_btn.addEventListener('click',()=>{
+    otp_send_btn.textContent='Resend';
+    otp_send_btn.style.backgroundColor='#dadada';
+    otp_send_btn.disabled=true;
+    otp_cont.style.display='flex';
+    otp_num=otpGenerator();
+    console.log(otp_num);
+    setTimeout(()=>{
+        otp_send_btn.disabled=false;
+        otp_send_btn.style.backgroundColor='var(--green-light)';
+    },3000);
+});
+
+otp_verify_btn.addEventListener('click',()=>{
+    if(+document.querySelector('#otp').value===otp_num){
+        input_ele[2].style.width='190px';
+        input_ele[2].disabled=true;
+        otp_send_btn.style.display='none';
+        otp_cont.style.display='none';
+    }
+    else{
+        document.querySelector('#otp').value=null;
+        otp_verify_btn.disabled=true;
+        otp_verify_btn.style.backgroundColor='#dadada';
+        setTimeout(()=>{
+            otp_verify_btn.disabled=false;
+            otp_verify_btn.style.backgroundColor='var(--green-light)';
+        },2000);
+    }
 })
 
