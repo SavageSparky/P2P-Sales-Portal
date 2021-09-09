3b6a89a32825e060e4966a8212b445845255668d
Jayashakthi28 <vishnu28.js@gmail.com>
2021-09-09 14:25:53 +0530

    added otp logic

--- public/js/profile-update.js
+++ public/js/profile-update.js
@@ -133,9 +133,9 @@ otp_send_btn.addEventListener('click',()=>{
     otp_send_btn.disabled=true;
     otp_cont.style.display='flex';
     otp_num=otpGenerator();
-    // fetch(`https://2factor.in/API/V1/934cf869-104d-11ec-a13b-0200cd936042/SMS/${input_ele[2].value}/${otp_num}`,{
-    //     method: 'POST'
-    // });
+    fetch(`https://2factor.in/API/V1/934cf869-104d-11ec-a13b-0200cd936042/SMS/${input_ele[2].value}/${otp_num}/P2P_Sales_Portal`,{
+        method: 'POST'
+    });
     console.log(otp_num);
     setTimeout(()=>{
         otp_send_btn.disabled=false;
