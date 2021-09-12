583cbcc8ea1d33cbeddb8e1247be96e2f79012c8
Savage Sparky <akilan73210@gmail.com>
2021-09-12 23:22:31 +0530

    started fetch functionality

--- public/js/landing_page.js
+++ public/js/landing_page.js
@@ -3,6 +3,7 @@ import { db_get,firebaseConfig,db_insert} from "./firebase-util.js";
 firebase.initializeApp(firebaseConfig);
 const db=firebase.database();
 const params = new URLSearchParams(window.location.search);
+console.log(params);
 const id = params.get("id");
 
 
@@ -16,6 +17,86 @@ firebase.auth().onAuthStateChanged(async (user) => {
     }
 });
 
+let data = await db_get(db, `product/${id}`);
+data = await data.val();
+console.log(data);
+document.querySelector('.basic_details').innerHTML = `
+<div class="img_container">
+    <div class="image_preview_wrap">
+        <img src=${data["profile-img"]} alt="" class="image_preview">
+    </div>
+    <div class="image_select">
+        <div class="image_nav_arrow">
+            <img src="../assets/icons/nav_arrow.svg" alt="" id="image_nav_arrow_left">
+        </div>
+        <div class="image_select_wrap">
+            <img class="img0" src=${data["profile-img"]} alt="">
+        </div>
+        <div class="image_select_wrap">
+            <img class="img1" src=${data["product-des-imgs[0]"]} alt="">
+        </div>
+        <div class="image_select_wrap">
+            <img class="img2" src=${data["product-des-imgs[1]"]} alt="">
+        </div>
+        <div class="image_select_wrap">
+            <img class="img3" src=${data["product-des-imgs[2]"]} alt="">
+        </div>
+        <div class="image_nav_arrow">
+            <img src="../assets/icons/nav_arrow.svg" alt="" id="image_nav_arrow_right">
+        </div>
+    </div>
+</div>
+<div class="product_details">
+    <h1 class="product_name">Ponni Rice Sack 75KG</h1>
+    <h3 class="product_price">Rs. 2000</h3>
+    <h3 class="product_due">Ends in: 30th September, 2021</h3>
+    <hr>
+    <ul class="detail_list">
+        <li>
+            <h4 class="detail_list_elements">Available: </h4>
+            <h4 id="detail_quantiy"> 20/200</h4>
+        </li>
+        <li>
+            <h4 class="detail_list_elements">Min-order: </h4>
+            <h4 id="detail_quantiy"> 10 units</h4>
+        </li>
+        <li>
+            <h4 class="detail_list_elements">Delivery:</h4>
+            <h4 id="delivery"> Not Available</h4>
+        </li>
+        <li>
+            <h4 class="detail_list_elements">Category: </h4>
+            <h4 id="detail_type"> Grains</h4>
+        </li>
+        <li>
+            <h4 class="detail_list_elements">Manufact. date: </h4>
+            <h4 id="detail_type"> March, 2021</h4>
+        </li>
+    </ul>
+    <hr>
+    <!-- <div class="product_address">
+        <h3 class="product_address_label">Address: </h4>
+        <h4 id="product_address_text">Street, Area, asadd, District, Pincode, State.</h4>
+    </div>
+    <hr> -->
+    <form class="product_buy">
+        <div class="quantity_wrapper">
+            <label for="quanity">Quantity</label>
+            <input type="number"
+            id="quantity" 
+            name="quantity" 
+            min="1"
+            required
+            >
+        </div> 
+        <h4 class="estimated_price">Estimated Price: Rs. 10,000</h4>
+        <button type="submit" class="buy_button">
+            Place Buy Request
+        </button>
+    </form>
+</div>
+`
+
 
 // image select and preview
 const image_wrap = document.querySelector(".image_preview_wrap>img");
@@ -26,4 +107,8 @@ image_select.addEventListener('click', (e)=>{
         const selected_img = e.target.src;
         image_wrap.src = selected_img;
     }
+    if(e.target.parentElement.className == 'image_nav_arrow') {
+        //  Code to be done later
+    }
 })
+
