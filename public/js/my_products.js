7d552c844c75e227b431b4963fcd1b43c211b249
Akilan <akilan73210@gmail.com>
2021-09-28 18:52:06 +0530

    layout fix

--- public/js/my_products.js
+++ public/js/my_products.js
@@ -1,10 +1,33 @@
+import { db_get, firebaseConfig } from "./firebase-util.js";
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+firebase.auth().onAuthStateChanged(async (user) => {
+  if (user) {
+    document.querySelector(".loading-cont").style.display = "none";
+    let user_id = user.uid;
+    let adone = await db_get(firebase.database(), `user/${user_id}`);
+    adone = adone.val();
+    console.log(adone);
+    if (adone === null) {
+      location.href = "/index.html";
+    }
+  } else {
+    document.querySelector(".loading-cont").style.display = "flex";
+    location.href = "/index.html";
+  }
+});
+
 
 // card click
 const cards = document.querySelectorAll('.card');
+const mainPanel = document.querySelector('.main_panel');
 const sidePanel = document.querySelector('.side_panel');
 cards.forEach((card) => {
     card.addEventListener('click', () => {
         sidePanel.classList.toggle('triggered');
+        mainPanel.classList.toggle('side_panel_space');
     })
 })
 
@@ -15,6 +38,3 @@ buyers.forEach( (buyer)=> {
         buyer.querySelector('.buyer_expand').classList.toggle('triggered');
     })
 })
-
-// new order link
-cosnt new_order = document.querySelector('.card_new_order');
